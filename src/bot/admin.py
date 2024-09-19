e2a1a38accec5fccbb0a682ad87d8d01009d85de
Eduard Gumen <hydrospirt@mail.ru>
2024-09-19 19:28:32 +0300

    Add CompanyAdmin, UserActivityAdmin

--- src/bot/admin.py
+++ src/bot/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
+from bot.models import Company, UserActivity
 from core.constants import SITE_HEADER, SITE_TITLE
 
 admin.site.site_header = SITE_HEADER
 admin.site.site_title = SITE_TITLE
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name", )
+
+
+@admin.register(UserActivity)
+class UserActivityAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user_id"
+        "username",
+        "company",
+        "join_time",
+        "leave_time",
+        "get_spent_time")
+    list_filter = ("company", "username")
+    search_fields = ("username", "company__name")
