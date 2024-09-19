5a1e568708b556508d5e4ae0a48ea88ba443d333
Eduard Gumen <hydrospirt@mail.ru>
2024-09-19 18:53:35 +0300

    Add verbose_name

--- src/bot/apps.py
+++ src/bot/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from core.constants import BOT_APP_VERBOSE
 
 
 class BotConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "bot"
+    verbose_name = BOT_APP_VERBOSE
