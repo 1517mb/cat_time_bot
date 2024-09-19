df6a835e78b1cedf382742990d717a8d95919cd4
Eduard Gumen <hydrospirt@mail.ru>
2024-09-19 19:09:07 +0300

    Add MAX_LEN and CompanyCfg

--- src/core/constants.py
+++ src/core/constants.py
@@ -2,7 +2,10 @@ VERSION = "0.0.1a"
 SITE_HEADER = f"Cat-Time-Bot {VERSION}"
 SITE_TITLE = "Панель управления"
 BOT_APP_VERBOSE = "Данные приложения Cat-Time-Bot"
+MAX_LEN = 255
 
 
 class CompanyCfg:
-    ...
+    VERBOSE_NAME = "Название"
+    META_NAME = "Организация"
+    META_PL_NAME = "Организации"
