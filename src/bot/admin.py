6ba54dec3c88993b63552276c90c8957600be878
Eduard Gumen <hydrospirt@mail.ru>
2024-09-19 19:05:51 +0300

    Add new site_header, site_title for django admin

--- src/bot/admin.py
+++ src/bot/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from core.constants import SITE_HEADER, SITE_TITLE
 
-# Register your models here.
+admin.site.site_header = SITE_HEADER
+admin.site.site_title = SITE_TITLE
