fa81444e1a741a6b1f061793e39e7b80eabb6532
Eduard Gumen <hydrospirt@mail.ru>
2024-09-19 19:09:27 +0300

    Add Company

--- src/bot/models.py
+++ src/bot/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from core.constants import MAX_LEN, CompanyCfg
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(
+        verbose_name=CompanyCfg.VERBOSE_NAME,
+        max_length=MAX_LEN)
+
+    class Meta:
+        verbose_name = CompanyCfg.META_NAME
+        verbose_name_plural = CompanyCfg.META_PL_NAME
+
+    def __str__(self) -> str:
+        return self.name
