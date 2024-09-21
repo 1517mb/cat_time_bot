5dceb32dc9134e986cdce8bce1bda954b4be12cc
Eduard Gumen <hydrospirt@mail.ru>
2024-09-21 15:48:40 +0300

    Add export_formats

--- src/bot/admin.py
+++ src/bot/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from import_export.admin import ExportMixin
+from import_export.formats import base_formats
+
 from bot.models import Company, UserActivity
+from bot.resources import UserActivityResource
 from core.constants import SITE_HEADER, SITE_TITLE
 
 admin.site.site_header = SITE_HEADER
@@ -13,7 +17,8 @@ class CompanyAdmin(admin.ModelAdmin):
 
 
 @admin.register(UserActivity)
-class UserActivityAdmin(admin.ModelAdmin):
+class UserActivityAdmin(ExportMixin, admin.ModelAdmin):
+    resource_class = UserActivityResource
     list_display = (
         "id",
         "username",
@@ -23,3 +28,10 @@ class UserActivityAdmin(admin.ModelAdmin):
         "get_spent_time")
     list_filter = ("company", "username")
     search_fields = ("username", "company__name")
+
+    def get_export_formats(self):
+        formats = (
+            base_formats.XLS,
+            base_formats.XLSX,
+        )
+        return [f for f in formats if f().can_export()]
