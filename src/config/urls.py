1a57f84119f70ff1b9f9695649b423afe6c3f70a
Eduard Gumen <hydrospirt@mail.ru>
2024-11-05 17:16:27 +0300

    Add unicron paths

--- src/config/urls.py
+++ src/config/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("catbot/", admin.site.urls),
+    path("unicorn/", include("django_unicorn.urls")),
     path("", include("generator.urls")),
 ]
