2efae9c84e11ae0b49a80791f6e1e918151163a9
Eduard Gumen <hydrospirt@mail.ru>
2024-12-16 19:38:38 +0300

    Add if DEBUG

--- src/config/settings.py
+++ src/config/settings.py
@@ -100,3 +100,9 @@ STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
