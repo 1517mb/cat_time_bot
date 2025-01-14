919fc8fb7dce71fc990c4971b604a15353538f59
Eduard Gumen <hydrospirt@mail.ru>
2025-01-14 19:41:09 +0300

    Replace sqlite database

--- src/config/settings.py
+++ src/config/settings.py
@@ -68,7 +68,7 @@ ASGI_APPLICATION = "config.asgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "data", "db.sqlite3"),
     }
 }
 
