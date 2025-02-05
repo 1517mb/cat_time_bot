72a4c735649046c75da18b05cf1124d960a1314d
Eduard Gumen <hydrospirt@mail.ru>
2025-02-05 20:30:59 +0300

    Update PasswordGeneratorForm with attrs for CSS

--- src/generator/forms.py
+++ src/generator/forms.py
@@ -2,6 +2,27 @@ from django import forms
 
 
 class PasswordGeneratorForm(forms.Form):
-    length = forms.IntegerField(initial=12, min_value=8, max_value=50)
-    include_digits = forms.BooleanField(required=False)
-    include_special_chars = forms.BooleanField(required=False)
+    length = forms.IntegerField(
+        initial=12,
+        min_value=8,
+        max_value=50,
+        widget=forms.NumberInput(attrs={
+            "class": "input",
+            "id": "length",
+        }),
+    )
+    include_digits = forms.BooleanField(
+        required=False,
+        initial=True,
+        widget=forms.CheckboxInput(attrs={
+            "class": "checkbox",
+            "id": "include_digits",
+        }),
+    )
+    include_special_chars = forms.BooleanField(
+        required=False,
+        widget=forms.CheckboxInput(attrs={
+            "class": "checkbox",
+            "id": "include_special_chars",
+        }),
+    )
