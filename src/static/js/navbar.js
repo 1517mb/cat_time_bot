b291eac64ef991d6e892e05fd1c47a4d4396f1b5
Eduard Gumen <hydrospirt@mail.ru>
2025-03-23 13:29:39 +0300

    Add check for target

--- src/static/js/navbar.js
+++ src/static/js/navbar.js
@@ -1,13 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-    
-    $navbarBurgers.forEach(el => {
-      el.addEventListener('click', () => {
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
-        
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-      });
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  $navbarBurgers.forEach(el => {
+    el.addEventListener('click', () => {
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      if (!$target) {
+        console.error('Target element not found:', target);
+        return;
+      }
+
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
     });
   });
+});
