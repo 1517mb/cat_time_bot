f409bba09ad7c0ed2c74b7d36c12b6ae6d1b635a
Eduard Gumen <hydrospirt@mail.ru>
2025-03-24 20:48:06 +0300

    Add ProgramAdmin

--- src/content/admin.py
+++ src/content/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Program
 
-# Register your models here.
+
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ("name", "downloads", "rating", "verified")
+    list_filter = ("verified", "created_at")
+    search_fields = ("name", "description")
+    readonly_fields = ("downloads", "created_at", "updated_at")
