5859ae5230da5afd9236886fc3f24b6541508058
Eduard Gumen <hydrospirt@mail.ru>
2025-04-08 20:16:41 +0300

    Add tip_detail path

--- src/generator/urls.py
+++ src/generator/urls.py
@@ -5,6 +5,7 @@ from generator import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("tips/", views.daily_tips_view, name="tips"),
+    path("tips/<int:pk>/", views.daily_tip_detail, name="tip_detail"),
     path("generate-password/",
          views.generate_password,
          name="generate_password"),
