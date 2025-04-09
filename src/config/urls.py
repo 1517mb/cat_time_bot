3206fa2c522252f4cabf5b774db93d4a6c75351b
Eduard Gumen <hydrospirt@mail.ru>
2025-04-09 22:20:36 +0300

    Add path for ckeditor5 and media for DEBUG

--- src/config/urls.py
+++ src/config/urls.py
@@ -6,5 +6,12 @@ from django.urls import include, path
 urlpatterns = [
     path("catbot/", admin.site.urls),
     path("markdownx/", include("markdownx.urls")),
+    path("ckeditor5/", include("django_ckeditor_5.urls")),
     path("", include("generator.urls")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
