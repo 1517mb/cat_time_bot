f6bf8ee69890f868450a9be31c5ae808c901d19a
Eduard Gumen <hydrospirt@mail.ru>
2025-04-10 21:16:28 +0300

    Add new err handlers

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,7 +1,12 @@
 from django.conf import settings
+from django.conf.urls import handler403, handler404, handler500
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+
+handler403 = "content.views.custom_403" # noqa
+handler404 = "content.views.custom_404" # noqa
+handler500 = "content.views.custom_500" # noqa
 
 urlpatterns = [
     path("catbot/", admin.site.urls),
@@ -9,6 +14,7 @@ urlpatterns = [
     path("ckeditor5/", include("django_ckeditor_5.urls")),
     path("", include("generator.urls")),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(
