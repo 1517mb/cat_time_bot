df055041d11b380922c2e94a717d277d3b6c2ad0
Eduard Gumen <hydrospirt@mail.ru>
2025-04-11 22:10:46 +0300

    Optimize the function cyrillic_slugify

--- src/core/utils.py
+++ src/core/utils.py
@@ -1,26 +1,16 @@
+import uuid
 from django.utils.text import slugify
 
 
 def cyrillic_slugify(s):
-    """Кастомная slugify с транслитерацией кириллицы"""
-    letters = {
-        "а": "a", "б": "b", "в": "v", "г": "g", "д": "d", "е": "e",
-        "ё": "yo", "ж": "zh", "з": "z", "и": "i", "й": "y", "к": "k",
-        "л": "l", "м": "m", "н": "n", "о": "o", "п": "p", "р": "r",
-        "с": "s", "т": "t", "у": "u", "ф": "f", "х": "h", "ц": "ts",
-        "ч": "ch", "ш": "sh", "щ": "shch", "ъ": "", "ы": "y", "ь": "",
-        "э": "e", "ю": "yu", "я": "ya",
-
-        "А": "A", "Б": "B", "В": "V", "Г": "G", "Д": "D", "Е": "E",
-        "Ё": "Yo", "Ж": "Zh", "З": "Z", "И": "I", "Й": "Y", "К": "K",
-        "Л": "L", "М": "M", "Н": "N", "О": "O", "П": "P", "Р": "R",
-        "С": "S", "Т": "T", "У": "U", "Ф": "F", "Х": "H", "Ц": "Ts",
-        "Ч": "Ch", "Ш": "Sh", "Щ": "Shch", "Ъ": "", "Ы": "Y", "Ь": "",
-        "Э": "E", "Ю": "Yu", "Я": "Ya"
+    """Транслитерация + обработка пустых значений"""
+    translit_map = {
+        "а": "a", "б": "b", "в": "v", "г": "g", "д": "d", "е": "e", "ё": "yo",
+        "ж": "zh", "з": "z", "и": "i", "й": "y", "к": "k", "л": "l", "м": "m",
+        "н": "n", "о": "o", "п": "p", "р": "r", "с": "s", "т": "t", "у": "u",
+        "ф": "f", "х": "h", "ц": "ts", "ч": "ch", "ш": "sh", "щ": "shch",
+        "ъ": "", "ы": "y", "ь": "", "э": "e", "ю": "yu", "я": "ya"
     }
-
-    result = []
-    for char in s:
-        result.append(letters.get(char, char))
-
-    return slugify("".join(result))
+    transliterated = "".join(translit_map.get(c.lower(), c) for c in str(s))
+    slug = slugify(transliterated)
+    return slug or f"untitled-{uuid.uuid4().hex[:6]}"
