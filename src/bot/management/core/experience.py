0c936b1c779825d27458868e7a0feb21409600aa
Eduard Gumen <hydrospirt@mail.ru>
2025-06-18 19:45:15 +0300

    Add calculate_experience

---
+++ src/bot/management/core/experience.py
@@ -0,0 +1,49 @@
+import re
+
+COMPANY_BONUSES = {
+    r'(?i)ум|ума|ум аркитекс': 50,
+    r'(?i)прогресс|фирма прогресс': 30,
+    r'(?i)инел|дисплей': 25,
+    r'(?i)тсн|tsn': 15,
+    r'(?i)имбиан|imbian|имбиан лаб': 10,
+}
+
+ACHIEVEMENT_BONUSES = {
+    "Первая кровь": 10,
+    "Ночная смена? Или просто забыл уйти?": 15,
+    "Кофеиновый марафонец": 10,
+    "Сова компании": 10,
+    "Лучший сотрудник": 30,
+    "Командный игрок": 15,
+    "А можно мне ещё выезд?": 10,
+    "Читер: Часовщик": -20,
+}
+
+
+def calculate_experience(activity, achievements) -> int:
+    """
+    Рассчитывает опыт для активности с учетом времени,
+    бонусов за организацию и достижений
+
+    :param activity: Объект UserActivity
+    :param achievements: Список названий достижений
+    :return: Количество опыта
+    """
+    base_exp = 10
+
+    time_spent = activity.leave_time - activity.join_time
+    time_exp = int(time_spent.total_seconds() / 300)
+
+    company_bonus = 0
+    company_name = activity.company.name
+    for pattern, bonus in COMPANY_BONUSES.items():
+        if re.search(pattern, company_name):
+            company_bonus = bonus
+            break
+
+    achievements_exp = 0
+    for achievement in achievements:
+        if achievement in ACHIEVEMENT_BONUSES:
+            achievements_exp += ACHIEVEMENT_BONUSES[achievement]
+
+    return base_exp + time_exp + company_bonus + achievements_exp
