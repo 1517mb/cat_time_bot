410d2f8c8ec588ee83b4859e2107ef1585984bbb
Eduard Gumen <hydrospirt@mail.ru>
2025-06-21 22:59:56 +0300

    Add create_progress_bar, code optimize

--- src/bot/management/core/utils.py
+++ src/bot/management/core/utils.py
@@ -4,10 +4,15 @@ from functools import lru_cache
 from asgiref.sync import sync_to_async
 
 
+def create_progress_bar(progress: float, length: int = 10) -> str:
+    filled = min(length, max(0, int(progress / 100 * length)))
+    return f"[{'■' * filled}{'□' * (length - filled)}]"
+
+
 @lru_cache(maxsize=365)
 def _is_holiday_sync(check_date: date) -> bool:
     """Проверяет, является ли дата выходным или праздником в России"""
-    if check_date.weekday() >= 5:
+    if check_date.weekday() >= 7:
         return True
 
     if check_date.month == 1 and 1 <= check_date.day <= 8:
