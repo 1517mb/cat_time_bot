cb905fb6549c14a2eec3b9cbd0d66e04cc17d532
Eduard Gumen <hydrospirt@mail.ru>
2025-08-09 14:34:15 +0300

    Add ProgramFilterForm

---
+++ src/content/forms.py
@@ -0,0 +1,45 @@
+from django import forms
+
+
+class ProgramFilterForm(forms.Form):
+    SEARCH_CHOICES = [
+        ("name", "По названию"),
+        ("description", "По описанию"),
+        ("all", "Везде")
+    ]
+
+    SORT_CHOICES = [
+        ("-created_at", "Новизне"),
+        ("-downloads", "Популярности"),
+        ("-rating", "Рейтингу")
+    ]
+
+    search = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={
+            "class": "input",
+            "placeholder": "Поиск..."
+        })
+    )
+    search_in = forms.ChoiceField(
+        choices=SEARCH_CHOICES,
+        initial="all",
+        widget=forms.Select(attrs={"class": "select"})
+    )
+    sort_by = forms.ChoiceField(
+        choices=SORT_CHOICES,
+        initial="-created_at",
+        widget=forms.Select(attrs={"class": "select"})
+    )
+    min_rating = forms.DecimalField(
+        required=False,
+        max_digits=3,
+        decimal_places=2,
+        widget=forms.NumberInput(attrs={
+            "class": "input",
+            "placeholder": "Мин. рейтинг",
+            "min": "0",
+            "max": "5",
+            "step": "0.1"
+        })
+    )
