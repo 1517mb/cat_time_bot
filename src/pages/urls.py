37afd3685080d4da68899190938ab411baa8701f
Eduard Gumen <hydrospirt@mail.ru>
2025-08-09 18:08:27 +0300

    Add new paths for static footer resources

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -8,4 +8,11 @@ urlpatterns = [
     path("privacy-policy/", views.PrivacyPolicyView.as_view(),
          name="privacy_policy"),
     path("terms-of-use/", views.TermsOfUseView.as_view(), name="terms_of_use"),
+    path("password-guide/", views.PasswordGuideView.as_view(),
+         name="password_guide"),
+    path("admin-checklist/", views.AdminChecklistView.as_view(),
+         name="admin_checklist"),
+    path("security-tools/", views.SecurityToolsView.as_view(),
+         name="security_tools"),
+    path("faq/", views.FAQView.as_view(), name="faq"),
 ]
