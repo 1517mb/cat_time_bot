54e3f754f93aae84eb0b53e032724ee036af1eb8
Eduard Gumen <hydrospirt@mail.ru>
2026-03-07 17:03:05 +0300

    Update version

--- src/core/constants.py
+++ src/core/constants.py
@@ -2,7 +2,7 @@
 
 from django.db.models import Index
 
-VERSION = "0.6.5 alpha"
+VERSION = "0.6.6 alpha"
 SITE_HEADER = f"Cat-Time-Bot {VERSION}"
 SITE_TITLE = "Панель управления"
 BOT_APP_VERBOSE = "Данные приложения Cat-Time-Bot"
