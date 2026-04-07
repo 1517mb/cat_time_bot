88bdec3f081e79fc1c740bab762736b4683ea482
Eduard Gumen <hydrospirt@mail.ru>
2026-04-07 20:11:00 +0300

    Add WAL init command

--- src/config/settings.py
+++ src/config/settings.py
@@ -92,6 +92,9 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "data", "db.sqlite3"),
+        "OPTIONS": {
+            "init_command": "PRAGMA journal_mode=WAL;",
+        }
     }
 }
 
